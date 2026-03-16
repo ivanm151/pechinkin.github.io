5693647f8bce92d9a534b4d26ef9843239274d00
ivanm151 <ivmedv5@gmail.com>
2026-03-17 01:25:40 +0700

    components

--- didj2/src/components/Picture.vue
+++ didj2/src/components/Picture.vue
@@ -1,11 +1,42 @@
 <script setup>
+import Circle from './Circle.vue'
 
+defineProps({
+  src: {
+    type: String,
+    required: true
+  },
+  alt: {
+    type: String,
+    default: 'Изображение'
+  },
+  caption: {
+    type: String,
+    default: ''
+  },
+  circleColor: {
+    type: String,
+    default: '#D62A2A'
+  }
+})
 </script>
 
 <template>
-
+  <div class="image-wrapper">
+    <img
+        :src="src"
+        :alt="alt"
+        class="image"
+    />
+    <Circle
+        v-if="caption"
+        :text="caption"
+        :color="circleColor"
+        class="caption-circle"
+    />
+  </div>
 </template>
 
-<style scoped>
-
+<style scoped lang="scss">
+@use '../assets/styles/components/picture';
 </style>
