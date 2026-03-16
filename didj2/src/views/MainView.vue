9bacada64131c45c65e5ddbb7d7c2754c66e63d8
ivanm151 <ivmedv5@gmail.com>
2026-03-16 17:58:45 +0700

    assets setup

---
+++ didj2/src/views/MainView.vue
@@ -0,0 +1,13 @@
+<script setup>
+
+</script>
+
+<template>
+<div>
+  VSEM PRIVET
+</div>
+</template>
+
+<style scoped>
+
+</style>
--- didj2/vite.config.js
+++ didj2/vite.config.js
@@ -1,7 +1,23 @@
+/*
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+})
+
+ */
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  server: {
+    host: '127.0.0.1',  // принудительно используем IPv4
+    port: 1025,
+    strictPort: true,    // не переключаться на другой порт если 5173 занят
+    open: true          // автоматически открыть браузер
+  }
 })
