5b894faf12e2b813a91246c25a2f54fef0d33fe5
ivanm151 <ivmedv5@gmail.com>
2026-03-17 13:04:14 +0700

    deploy fix

--- didj2/vite.config.js
+++ didj2/vite.config.js
@@ -5,13 +5,13 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [vue()],
 
-
+/*
   server: {
     host: '127.0.0.1',  // принудительно используем IPv4
     port: 1025,
     strictPort: true,    // не переключаться на другой порт если 5173 занят
     open: true          // автоматически открыть браузер
   }
-
+*/
 
 })
